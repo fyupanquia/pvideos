00f139bd4d71c8fc6c6ec8f5a8b802d40daeaca3
fyupanquia <fyupanquia@outlook.com>
2020-06-29 16:20:59 -0500

    server

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
           priority: 1,
           filename: isDev ? 'assets/vendor.js' : 'assets/vendor-[hash].js',
           enforce: true,
-          test (module, chunks) {
+          test(module, chunks) {
             const name = module.nameForCondition && module.nameForCondition()
             return chunks.some(
               chunk => chunk.name !== 'vendors' && /[\\/]node_modules[\\/]/.test(name)
@@ -94,9 +94,9 @@ module.exports = {
   },
   plugins: [
     isDev ? new webpack.HotModuleReplacementPlugin() : () => {},
-    isDev
-      ? () => {}
-      : new CompressionWebpackPlugin({
+    isDev ?
+      () => {} :
+      new CompressionWebpackPlugin({
         test: /\.js$|\.css$/,
         filename: '[path].gz'
       }),
